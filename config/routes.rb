ac7a8cf7caa3751a83e1b78fc78d033cc9c97055
Stephen Wentling <swentling87@gmail.com>
2016-05-15 14:18:11 -0700

    date time and ajax

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:show] do
-    resources :items, only: [:create, :new]
+    resources :items, only: [:create, :new, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
